1184dd49474cd5caabefec007ee131dd36f6129c
mohammedbahnini <mohammed.gosoft@gmail.com>
2024-04-17 00:41:42 +0100

    add final images

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,53 @@ const Page = React.forwardRef((props, ref) => {
   );
 });
 
-const cover = '/book/cover.PNG';
+const cover = '/pages/1.webp';
+const end = '/pages/25.webp';
 const images = [
-  '/book/page1.PNG',
-  '/book/page2.PNG',
-  '/book/page3.PNG',
-  '/book/page4.PNG',
-  '/book/page5.PNG',
-  '/book/page6.PNG',
-  '/book/page7.PNG',
-  '/book/page8.PNG'
+  '/pages/2.webp',
+  '/pages/3.webp',
+  '/pages/4.webp',
+  '/pages/5.webp',
+  '/pages/6.webp',
+  '/pages/7.webp',
+  '/pages/8.webp',
+  '/pages/9.webp',
+  '/pages/10.webp',
+  '/pages/11.webp',
+  '/pages/12.webp',
+  '/pages/13.webp',
+  '/pages/14.webp',
+  '/pages/15.webp',
+  '/pages/16.webp',
+  '/pages/17.webp',
+  '/pages/18.webp',
+  '/pages/19.webp',
+  '/pages/20.webp',
+  '/pages/21.webp',
+  '/pages/22.webp',
+  '/pages/23.webp',
+  '/pages/24.webp',
 ];
 
 
 export class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      page: 0,
+      totalPage: 0
+    };
   }
+
+
 
 
   nextButtonClick = () => {
 
     this.flipBook.pageFlip().flipNext('bottom');
+
+
   };
 
   prevButtonClick = () => {
@@ -45,57 +70,65 @@ export class App extends Component {
 
     return (
       <>
-        <div className='container' style={{ overflow: 'hidden' , width : '900px' }}>
-          
+        <div className='container' style={{ overflow: 'hidden', width: '900px' }}>
+
           <div className='book' >
             <HTMLFlipBook
-             width={700}
-             height={980}
-             size="stretch"
-             minWidth={315}
-             maxWidth={1000}
-             minHeight={400}
-             maxHeight={1533}
-             maxShadowOpacity={0.5}
-             showCover={true}
-             mobileScrollSupport={true}
-   
+              width={700}
+              height={980}
+              size="stretch"
+              minWidth={315}
+              maxWidth={1000}
+              minHeight={400}
+              maxHeight={1533}
+              maxShadowOpacity={0.5}
+              showCover={true}
+              mobileScrollSupport={true}
               ref={(el) => (this.flipBook = el)}
-
             >
               <div className="page page-cover" data-density="hard">
-                <img src={cover} alt="" />
+                <div className='page-content'>
+                  <img src={cover} alt="" />
+                </div>
               </div>
-              {images.map(item => {
-                return (
-                  <div className="page">
-                    <img src={item} alt="" />
-                  </div>
+              {
+                images.map((item, index) => {
+                  return (
+                    <div className="page" key={index}>
+                      <div className="page-content">
+                        <img src={item} alt="" />
+                      </div>
+                    </div>
 
-                )
-              })}
-              
+                  )
+                })
+              }
 
-
+              <div className="page" >
+                <div className="page-content" style={{width : '100%' , height : '100%' ,  backgroundColor : 'white'}}>
+                  
+                </div>
+              </div>
 
               <div className="page page-cover" data-density="hard">
                 <div className='page-content'>
-
+                  <img src={end} alt="" />
                 </div>
               </div>
+
 
             </HTMLFlipBook>
           </div>
 
 
           <div className="controls">
+            
             <button type="button" onClick={this.prevButtonClick}>
-              Previous page
+            الصفحة السابقة
             </button>
 
-
             <button type="button" onClick={this.nextButtonClick}>
-              Next page
+            الصفحة الموالية
             </button>
           </div>
 
